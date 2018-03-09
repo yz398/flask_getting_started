a9f824b0f2150a7a53da4529742568d48003fd81
VCM admin account <vcm@vcm-3551.vm.duke.edu>
2018-03-08 22:31:58 -0500

    change

--- client.py
+++ client.py
@@ -1,11 +1,11 @@
 import requests
-r = requests.get("vcm-3551.vm.duke.edu:5000/name")
+r = requests.get("http://vcm-3551.vm.duke.edu:5000/name")
 name = r.json()
 
-r1 =requests.get("vcm-3461.vm.duke.edu:5000/hello/Yi Zhao")
+r1 =requests.get("http://vcm-3551.vm.duke.edu:5000/hello/Yi Zhao")
 hello =r1.json()
 
-r2 = requests.post("vcm-3551.vm.duke.edu:5000/distance",json={"a":[2,5],"b":[4,5]})
+r2 = requests.post("http://vcm-3551.vm.duke.edu:5000/distance",json={"a":[2,5],"b":[4,5]})
 distance = r2.json()
 print("The response was {0}.".format(distance['distance']))
 
